0136c32f71aaf15f725704154a16771da6858d46
Norman Lynch <lynchnf@gmail.com>
2021-02-02 17:51:01 -0600

    Finish converting to multi module project.

--- flunky-main/src/main/java/norman/flunky/main/Main.java
+++ flunky-main/src/main/java/norman/flunky/main/Main.java
@@ -3,7 +3,6 @@ package norman.flunky.main;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
-import freemarker.template.TemplateExceptionHandler;
 import norman.flunky.api.GenerationBean;
 import norman.flunky.api.ProjectType;
 import norman.flunky.api.TemplateType;
@@ -13,11 +12,11 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
@@ -32,37 +31,37 @@ public class Main {
         // Convert applications properties file to a bean.
         ApplicationBean appBean = new ApplicationBean(args[0]);
         ProjectType type = appBean.getProjectType();
+        // TODO Change name to getTemplatePrefix and return String.
+        String tmpPrefix = type.name();
 
-        // Create Freemarker configuration for paths.
-        Configuration pthCfg = new Configuration(Configuration.VERSION_2_3_30);
+        // Create Freemarker configuration.
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_30);
 
-        // Create Freemarker configuration for templates.
-        File tmpDir = getTemplateDir(type);
-        Configuration tmpCfg = createTemplateConfiguration(tmpDir);
+        // Get context class loader so we can use it to get resources, i.e. template files that are in a jar file.
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
 
         // Create project directory.
-        File projectDirectory = appBean.getProjectDirectory();
-        File projDir = createProjDir(projectDirectory);
+        File projDir = createProjDir(appBean.getProjectDirectory());
         LOGGER.info("Creating project in directory " + projDir + ".");
 
         // Create application source files.
         Map<String, Object> appModel = appBean.getApplicationModel();
         for (GenerationBean appGenBean : type.getApplicationGenerationProperties()) {
-            createSourceFile(pthCfg, tmpDir, tmpCfg, projDir, appModel, appGenBean);
+            createSourceFile(cfg, loader, tmpPrefix, projDir, appModel, appGenBean);
         }
 
         // Create enum source files.
         List<Map<String, Object>> enumModels = appBean.getEnumModels();
         for (Map<String, Object> enumModel : enumModels) {
             for (GenerationBean enumGenBean : type.getEnumGenerationProperties()) {
-                createSourceFile(pthCfg, tmpDir, tmpCfg, projDir, enumModel, enumGenBean);
+                createSourceFile(cfg, loader, tmpPrefix, projDir, enumModel, enumGenBean);
             }
         }
 
         // Create entity source files.
         for (Map<String, Object> entModel : appBean.getEntityModels()) {
             for (GenerationBean entGenBean : type.getEntityGenerationProperties()) {
-                createSourceFile(pthCfg, tmpDir, tmpCfg, projDir, entModel, entGenBean);
+                createSourceFile(cfg, loader, tmpPrefix, projDir, entModel, entGenBean);
             }
         }
         LOGGER.info("Project created successfully!");
@@ -74,6 +73,8 @@ public class Main {
         }
     }
 
+/*
+    @Deprecated
     private static File getTemplateDir(ProjectType type) {
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
         URL resource = contextClassLoader.getResource("templates/readme.txt");
@@ -85,7 +86,10 @@ public class Main {
             throw new LoggingException(LOGGER, "Unable to resolve template directory.", e);
         }
     }
+*/
 
+/*
+    @Deprecated
     private static Configuration createTemplateConfiguration(File tmpDir) {
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_30);
         try {
@@ -102,6 +106,7 @@ public class Main {
             throw new LoggingException(LOGGER, "Unable to set template loading directory " + tmpDir + ".", e);
         }
     }
+*/
 
     private static File createProjDir(File dir) {
         if (!dir.mkdirs()) {
@@ -110,15 +115,15 @@ public class Main {
         return dir;
     }
 
-    private static void createSourceFile(Configuration pthCfg, File tmpDir, Configuration tmpCfg, File projDir,
+    private static void createSourceFile(Configuration cfg, ClassLoader loader, String tmpPrefix, File projDir,
             Map<String, Object> dataModel, GenerationBean genBean) {
-        String outPath = generatePath(pthCfg, genBean.getOutputFilePath(), dataModel);
+        String outPath = generatePath(cfg, genBean.getOutputFilePath(), dataModel);
         File outFile = createOutputFile(projDir, outPath);
         String templateName = genBean.getTemplateName();
         if (genBean.getTemplateType() == TemplateType.COPY) {
-            copySourceFile(tmpDir, templateName, outFile);
+            copySourceFile(loader, tmpPrefix, templateName, outFile);
         } else {
-            generateSourceFile(tmpCfg, templateName, outFile, dataModel);
+            generateSourceFile(cfg, loader, tmpPrefix, templateName, outFile, dataModel);
         }
         LOGGER.info("Created source file " + outFile + ".");
     }
@@ -143,26 +148,43 @@ public class Main {
         return file;
     }
 
-    private static void copySourceFile(File tmpDir, String tmpName, File outFile) {
-        File templateFile = new File(tmpDir, tmpName);
+    private static void copySourceFile(ClassLoader loader, String tmpPrefix, String tmpName, File outFile) {
+        InputStream template = null;
         try {
-            Files.copy(templateFile.toPath(), outFile.toPath());
+            template = loader.getResourceAsStream(tmpPrefix + "/" + tmpName);
+            Files.copy(template, outFile.toPath());
         } catch (IOException e) {
-            throw new LoggingException(LOGGER, "Unable to copy file " + templateFile + " to file " + outFile + ".", e);
+            throw new LoggingException(LOGGER, "Unable to copy template " + tmpName + " to file " + outFile + ".", e);
+        } finally {
+            if (template != null) {
+                try {
+                    template.close();
+                } catch (IOException e) {
+                    LOGGER.warn("Unable to close input stream for template " + tmpName + ".");
+                }
+            }
         }
     }
 
-    private static void generateSourceFile(Configuration cfg, String tmpName, File outFile,
-            Map<String, Object> dataModel) {
+    private static void generateSourceFile(Configuration cfg, ClassLoader loader, String tmpPrefix, String tmpName,
+            File outFile, Map<String, Object> dataModel) {
+        InputStream template = null;
         Writer writer = null;
         try {
-            Template temp = cfg.getTemplate(tmpName);
+            template = loader.getResourceAsStream(tmpPrefix + "/" + tmpName);
             writer = new FileWriter(outFile);
-            temp.process(dataModel, writer);
+            Template tmp = new Template("name", new InputStreamReader(template), cfg);
+            tmp.process(dataModel, writer);
         } catch (IOException | TemplateException e) {
-            throw new LoggingException(LOGGER, "Unable to process template " + tmpName + " to file " + outFile + ".",
-                    e);
+            e.printStackTrace();
         } finally {
+            if (template != null) {
+                try {
+                    template.close();
+                } catch (IOException e) {
+                    LOGGER.warn("Unable to close input stream for template " + tmpName + ".");
+                }
+            }
             if (writer != null) {
                 try {
                     writer.close();
