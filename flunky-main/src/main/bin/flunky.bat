4425a2f5d00748a59e55503cb3323f2af981250f
Norman Lynch <norman_lynch@redmane.com>
2025-07-17 08:13:19 -0500

    Fix Windows batch file.

--- flunky-main/src/main/bin/flunky.bat
+++ flunky-main/src/main/bin/flunky.bat
@@ -1,6 +1,15 @@
 @echo off
 if "%OS%" == "Windows_NT" setlocal
+if "%1" == "" goto noparm
 set CURRENT_DIR=%cd%
-cd /d %0\..
-start "${project.name}" /B java -jar ..\lib\${pom.artifactId}-${pom.version}.jar
+cd /d %0\..\..
+
+start "${project.parent.name}" /B java -cp ${project.artifactId}-${project.version}.${project.packaging};lib/* norman.flunky.main.Main %1
+
 cd %CURRENT_DIR%
+goto end
+
+:noparm
+echo Usage: %0 ^<path-to-properties-file^>
+
+:end
